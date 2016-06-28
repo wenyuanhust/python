99e82df1bf5b6517713f2afec37df3fb8aae9729
wen <wenyuanhust@qq.com>
2016-06-28 20:00:12 +0800

    all files

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 #class Hello(object):
 #	def hello(self, name = 'world'):
+print "x"
+print 'y'
+
 print "hello"#% name)
--- print.py
+++ print.py
@@ -1,1 +1,2 @@
-print('hello, world')
+name = input()
+print('hello, %s', name)
